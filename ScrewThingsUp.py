065f530cb9c784b459793ecfb4be7ed03992da46
HW1 <harrison.white@gmail.com>
2019-07-13 10:43:43 +0100

    First commit

---
+++ ScrewThingsUp.py
@@ -0,0 +1,10 @@
+
+# 1.2.4 review exercises
+
+''' The following line won't run because of a syntax error '''
+print("hi)
+
+''' The following lines won't run properly,
+    even if the syntax error in the line above is corrected,
+    because of a run-time error '''
+print(hello)
