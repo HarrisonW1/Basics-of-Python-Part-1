02afa485092a89634fc3107b02656f07575ccc3e
HW1 <harrison.white@gmail.com>
2019-07-14 15:04:27 +0100

    First commit

---
+++ UseObjectsAndMethods.py
@@ -0,0 +1,6 @@
+#Write a script that takes input from the user and displays that input back
+test = input("Say something and I will repeat it exactly: ")
+print(test)
+#Use CTRL+SPACE to view all the methods of a string object, then write a script that returns the lower-case version of a string
+ test = input("Say something and I will repeat it exactly: ")
+ print(test.lower())
