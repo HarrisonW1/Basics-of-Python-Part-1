5d11e1384ed26be7d5558a3d8b45b0c9b48ed3f4
HW1 <harrison.white@gmail.com>
2019-07-14 13:36:52 +0100

    First commit.

---
+++ MessAroundWithWords.py
@@ -0,0 +1,19 @@
+#Create a string and print its length using the len() function
+word = "Harrison"
+print(len(word))
+#Create two strings, concatenate them (add them next to each other) and print
+#the combination of the two strings
+word1 = "Ha"
+word2 = "rry"
+word3 = word1 + word2
+print(word3)
+#Create two string variables, then print one of them after the other
+#(with a space added in between) using a comma in your print statement
+word1 = "Ha"
+word2 = "rry"
+word4 = word1 + ", " + word2 + "."
+print(word4)
+#print the string "zing" by using subscripting and index numbers on the string
+#"bazinga" to specify the correct range of characters
+target = "bazinga"
+print(target[2:6])
