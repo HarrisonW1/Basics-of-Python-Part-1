967a1b24f0734a1e4dbff695002d5a6f0d6d01eb
HW1 <harrison.white@gmail.com>
2019-07-14 19:51:28 +0100

    First commit.

---
+++ Find a String in a String.py
@@ -0,0 +1,14 @@
+# Cannot find the string "a" in the string "AAA":
+print("AAA".find("a"))
+
+
+# Try to find a number inside a string;
+# use str() to convert the number first
+version = "version 2.0"
+v_num = 2.0
+print(version.find(str(v_num)))
+
+
+# Try to find an upper-case "X" in user input:
+my_input = input("Type something: ")
+print(my_input.find("X"))
